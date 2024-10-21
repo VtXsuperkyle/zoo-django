8bec0b8e8919e9acda490a5216a23000c119c59e
VtXsuperkyle <160232706@student.rnngroup.ac.uk>
2024-10-21 16:33:58 +0100

    slide 41

---
+++ dcrm/website/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def home(request):
+    return render(request, 'website/index.html')
+
+def register(request):
+    return render(request, 'website/register.html')
