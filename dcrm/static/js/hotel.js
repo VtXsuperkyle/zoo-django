44180475f1e0abd075859a6433b585fa1199e923
VtXsuperkyle <160232706@student.rnngroup.ac.uk>
2024-11-29 14:36:55 +0000

    done

--- dcrm/static/js/hotel.js
+++ dcrm/static/js/hotel.js
@@ -1,36 +1,49 @@
 function update_cost(){
     // inform the user of the cost of choice
+    document.getElementById("booking_button").disabled = true;
 
     // get dates
     let arrive = document.getElementById('id_hotel_booking_date_arrive').value
    let leave = document.getElementById('id_hotel_booking_date_leave').value
-
+   let price = document.getElementById('hotel_output')
    // calculate number of days
    arrive = new Date(arrive)
    leave = new Date(leave)
    diff = leave.getTime() - arrive.getTime();
-   days = Math.round(Math.abs(diff/(1000*60*60*24)));
-
+   days = Math.round(diff/(1000*60*60*24));
+   console.log("Days")
    // check the days is valid
    if (String(days) == "NaN"){
-    let price = document.getElementById('hotel_output')
+
     price.innerHTML = "Hotel cost: Dates Not Chosen"
+   
    }else{
 
-    // calculate costs
-    let total = parseInt(adults.value) * 65
+    if (days > 0){
+        if (adults.value + children.value + oaps.value > 0){
+                // calculate costs
+                let total = parseInt(adults.value) * 65
                 + parseInt(children.value) * 35
                 + parseInt(oaps.value) * 45
-    
-    //multiply by number of nights booked
-    total = total * days
+
+            //multiply by number of nights booked
+            total = total * days
 
 
-    //console.log("Total:" + String(total))
+            //console.log("Total:" + String(total))
 
-    // update the display for the user
-    let price = document.getElementById('hotel_output')
-    price.innerHTML = "Hotel cost: £" + String(total)
+            // update the display for the user
+
+            price.innerHTML = "Hotel cost: £" + String(total)
+            document.getElementById("booking_button").disabled = false;
+
+        }else{
+
+            price.innerHTML = "A least 1 adult should be chosen"
+        }
+    }else{
+        price.innerHTML = "Date not acceptable"
+    }
    }
 }
 
@@ -43,3 +56,8 @@ let oaps = document.getElementById("id_hotel_booking_oap");
 oaps.addEventListener("change", update_cost)
 
 // TO DO: add the same updates for the date fields
+let arrive_date = document.getElementById("id_hotel_booking_date_arrive");
+arrive_date.addEventListener("change", update_cost)
+
+let leave_date = document.getElementById("id_hotel_booking_date_leave");
+leave_date.addEventListener("change", update_cost)
